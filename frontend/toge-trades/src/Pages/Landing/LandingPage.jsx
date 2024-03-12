ff294a7d8963f44155739a4e203bc47436b725fb
Rachel Nataatmadja <nataatmadjarachel@gmail.com>
2024-03-13 02:16:01 +1300

    FEAT KB-1: Added togepi image and resize responsiveness

--- frontend/toge-trades/src/Pages/Landing/LandingPage.jsx
+++ frontend/toge-trades/src/Pages/Landing/LandingPage.jsx
@@ -1,19 +1,28 @@
-import { useContext } from 'react';
-import { AppContext } from '../../AppContextProvider';
-import LandingHeader from '../../components/landing/landing-header/LandingHeader';
-import './LandingPage.css';
-import LandingBackground from '../../components/landing/landing-background/LandingBackground';
+import { useContext } from "react";
+import { AppContext } from "../../AppContextProvider";
+import LandingHeader from "../../components/landing/landing-header/LandingHeader";
+import "./LandingPage.css";
+import LandingBackground from "../../components/landing/landing-background/LandingBackground";
+import LandingImage from "../../components/landing/landing-image/LandingImage";
 
 export default function LandingPage() {
-
-    return (
-        <div className='landing-page'>
-            <LandingHeader />
-            <div className='content'>
-                <h1 className='headline'>Gotta Hatch 'Em All!</h1>
-                <b className='subtitle'>Hatch eggs and trade Pokemon to complete your Pokedex.</b>
-            </div>
-            <LandingBackground />
+  return (
+    <div className="landing-page">
+      <LandingHeader />
+      <div className="content">
+        <div className="text-content">
+          <h1 className="headline">Gotta Hatch 'Em All!</h1>
+          <p className="subtitle">
+            Hatch eggs and trade Pokemon to complete your Pokedex.
+          </p>
+          <div className="button-horizontal">
+            <button className="login-btn">Login</button>
+            <button className="signup-btn">Sign up</button>
+          </div>
         </div>
-    );
+        <LandingImage />
+      </div>
+      <LandingBackground />
+    </div>
+  );
 }
