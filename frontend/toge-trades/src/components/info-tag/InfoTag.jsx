0fa2e31e8aca772f0664e0962ca350a38ee84507
Rachel Nataatmadja <nataatmadjarachel@gmail.com>
2024-04-09 21:25:28 +1200

    FEAT KB-6: Modified info tag css

--- frontend/toge-trades/src/components/info-tag/InfoTag.jsx
+++ frontend/toge-trades/src/components/info-tag/InfoTag.jsx
@@ -1,9 +1,9 @@
 import "./InfoTag.css";
-
+// For height and weight tags
 export default function InfoTag({ title, subtitle }) {
   return (
     <div className="infotag-container">
-      <h3>{title}</h3>
+      <h3 className="infotag-title">{title}</h3>
       <div className="infotag-subtitle">
         <p>{subtitle}</p>
       </div>
