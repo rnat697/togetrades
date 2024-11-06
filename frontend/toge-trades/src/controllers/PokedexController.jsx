1efc373868e4d38ab72dc5bac31746a60d93d1a9
Rachel Nataatmadja <nataatmadjarachel@gmail.com>
2024-11-06 13:17:12 +1300

    FEAT KB-17: Added basic files for Pokedex

---
+++ frontend/toge-trades/src/controllers/PokedexController.jsx
@@ -0,0 +1,6 @@
+import useGet from "../hooks/useGet";
+import { useState } from "react";
+import SpeciesModel from "../models/SpeciesModel";
+
+// --- Fetches Species for pokedex ---
+export function usePokedex(currentPage) {}
