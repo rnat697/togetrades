58929145b571233871e12a4867aa6a91bc2c5630
Rachel Nataatmadja <nataatmadjarachel@gmail.com>
2024-11-17 19:42:25 +1300

    FEAT KB-30: Added API endpoints in frontend for fetching/adding/removing wishlist

--- frontend/toge-trades/src/models/UserModel.jsx
+++ frontend/toge-trades/src/models/UserModel.jsx
@@ -1,12 +1,23 @@
+import Species from "../../../../backend/src/db/species-schema";
+
 class UserModel {
-  constructor(id, username, email, image) {
+  constructor(id, username, email, image, wishlist) {
     this.id = id;
     this.username = username;
     this.email = email || null;
     this.image = image || null;
+    this.wishlist = wishlist
+      ? wishlist.map((species) => Species.fromJSON(species))
+      : [];
   }
   static fromJSON(data) {
-    return new UserModel(data._id, data.username, data.email, data.image);
+    return new UserModel(
+      data._id,
+      data.username,
+      data.email,
+      data.image,
+      data.wishlist
+    );
   }
 }
 export default UserModel;
